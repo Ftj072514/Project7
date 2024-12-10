4411eb6c911e01ec0b5c52c2785287eff9f08e83
ftj <ftj072514@gmail.com>
2024-12-10 20:58:36 +0800

    initialization

---
+++ SkipList.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int val;
+    int level;
+    struct node* next;
+}typedef Node;
+
+int randLevel(){
+    int num;
+    return num;
+}
+//list[] is an array to store the skip list, and each element list[i] represents a level of the skip list
+void Search(Node* list[], int val, int level){   //level represents the valid number of levels in list[]
+    return;
+}
+
+void Insert(Node* list[], int val, int level){
+    return;
+}
+
+void Delete(Node* list[], int val, int level){
+    return;
+}
